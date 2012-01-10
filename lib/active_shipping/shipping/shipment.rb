7aa3c8b85d4910a8d0b3820e27c5d874c1d318cd
Kelly Mahan <kmahan@kmahan.com>
2012-01-10 16:12:43 -0600

    removed rate data and now able to get rid of spawn. Still need to drop nokogiri to be included in active_shipping

--- lib/active_shipping/shipping/shipment.rb
+++ lib/active_shipping/shipping/shipment.rb
@@ -9,7 +9,6 @@ module ActiveMerchant #:nodoc:
       class RequiredOptionError < StandardError; end
       class SaveLabelError < StandardError; end
       class VerifyRatesError < StandardError; end
-      include Spawn
       #required attributes
       attr_accessor :ship_to, :shipper, :carrier, :options, :rate_xml, :label_xml, :tracking, :postage, :error
       #attr_accessor :service_type_code #available options in ActiveShipping::Shipping::Ups::DEFAULT_SERVICES
@@ -71,92 +70,7 @@ module ActiveMerchant #:nodoc:
       def international?
         self.ship_to.country != self.shipper.country
       end
-      
-      def get_rates
-        case
-        when @carrier.class == ActiveMerchant::Shipping::Endicia          
-          rates = []
-          if self.service_type_code == "All"
-            spawn_ids = []
-            spawn_method = :thread
-            ActiveMerchant::Shipping::Endicia::US_SERVICES.each_pair do |key, value|
-              unless self.packages[0].ounces>13 && key == :first_class
-                @carrier.box_types(key).each do |box|
-                  flat_rates = @carrier.flat_rates(value, box)
-                  if flat_rates
-                    rates << flat_rates
-                  else
-                    spawn_ids << spawn(:method => spawn_method) do
-                      x = 0
-                      self.packages[0].package_type = box
-                      self.service_type_code = value
-                      @rate_xml = @carrier.find_rates(self, @options)
-                      doc = Hpricot(@rate_xml)
-                      service = "USPS  - #{(doc/'postageprice'/'postage'/'mailservice').inner_text}"
-                      rate = doc.at('postageprice') ? doc.at('postageprice')['totalamount'] : nil
-                      if error_message = doc.at('errormessage')
-                        service = "Error : USPS - #{value}: #{error_message.inner_text}"
-                        puts "************************************************"
-                        rates << [service, 0]
-                        puts service
-                        x += 1
-                      else
-                        rates << [service, rate]
-                        x = 20
-                      end
-                    end
-                  end
-                end
-              end
-            end
-            wait(spawn_ids)
-          end
-          return rates.sort{|a,b| a[1].to_f<=>b[1].to_f}
-        when @carrier.class == ActiveMerchant::Shipping::FedEx
-          rates = []
-          @rate_xml = @carrier.find_rates(self, @options)
-          
-          puts "************************************************"
-          puts @carrier.last_request
-          puts "************************************************"
-          puts @rate_xml
-          doc = Nokogiri::XML::Document.parse(@rate_xml)
-          
-          
-          if doc.search('//Error/Message').empty?
-            ship_types = doc.search('//Entry/Service').map { |st| st.content }
-            totals = doc.search('//Entry/EstimatedCharges/DiscountedCharges/NetCharge').map { |p| p.content rescue "none"}
-            ship_types.each_with_index { |st, i| rates << ["FEDEX - #{st.to_s}", totals[i]] }
-          else
-            error_message = doc.search('//Error/Message')
-            service = "Fedex Error : #{error_message.first.content}"
-            puts "************************************************"
-            rates << [service, 0]
-            puts service
-          end
-          
-          return rates.uniq.sort{|a,b| a[1].to_f<=>b[1].to_f}
-        else
-          puts "couldnt find carrier #{@carrier}"
-          return nil
-        end
-      end
-      
-      def verify_rates
-        case @carrier
-        when ActiveMerchant::Shipping::USPS
-          return nil
-        else
-          @rate_xml, success, message = @carrier.find_shipping_info(self, @options) #returns xml, success, message
-        
-          if success
-            return @rate_xml
-          else
-            raise VerifyRatesError, "<error><message>#{message}</message><xml>#{@rate_xml}</xml><request>#{@carrier.last_request}</request></error>"
-          end
-        end
-      end
-      
+
       def request_label(shipmentdigest = nil)
         case @carrier
         when ActiveMerchant::Shipping::USPS
